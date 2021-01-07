eec802800af9a2885ab46fca6cb3f78305618a9c
Anikesh Sinha <anikeshsinha041198@gmail.com>
2021-01-07 18:20:00 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import WbIncandescentOutlinedIcon from '@material-ui/icons/WbIncandescentOutlined';
+
+function Header() {
+  return (
+    <header>
+      
+      <h1><WbIncandescentOutlinedIcon/> PeeK</h1>
+    </header>
+  );
+}
+
+export default Header;
